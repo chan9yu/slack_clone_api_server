dfad3d3468197411a4023bd781420571a6aa8560
chan9yu <wry5533@gmail.com>
2023-10-02 21:43:23 +0900

    feat: project init

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { config } from 'dotenv';
+
+import { AppModule } from './app.module';
+
+config();
+
+const API_PREFIX = 'api' as const;
+const PORT = process.env.PORT || 3333;
+
+const corsOptions: CorsOptions = {
+	origin: [process.env.APP_URL, process.env.DEV_URL || 'http://localhost:3035'],
+	credentials: true,
+	methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+	preflightContinue: false,
+	optionsSuccessStatus: 204
+};
+
+async function bootstrap() {
+	const app = await NestFactory.create<NestExpressApplication>(AppModule);
+	app.enableCors(corsOptions);
+	app.setGlobalPrefix(API_PREFIX);
+	app.useGlobalPipes(new ValidationPipe());
+
+	await app.listen(PORT);
+	Logger.log(`🚀 어플리케이션을 시작합니다 http://localhost:${PORT}/${API_PREFIX}`);
+}
+
+bootstrap();
