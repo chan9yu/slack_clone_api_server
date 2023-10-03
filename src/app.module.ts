2425c392b411234c411031af5e06944530fd397e
chan9yu <wry5533@gmail.com>
2023-10-03 18:03:32 +0900

    feat: 도메인별 모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
+import { appConfig } from './app.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { appConfig } from './app.config';
+import { ChannelsModule } from './channels/channels.module';
+import { DmsModule } from './dms/dms.module';
+import { UsersModule } from './users/users.module';
+import { WorkspacesModule } from './workspaces/workspaces.module';
 
 @Module({
-	imports: [ConfigModule.forRoot({ cache: true, isGlobal: true, load: [appConfig] })],
+	imports: [
+		ConfigModule.forRoot({ cache: true, isGlobal: true, load: [appConfig] }),
+		ChannelsModule,
+		DmsModule,
+		UsersModule,
+		WorkspacesModule
+	],
 	controllers: [AppController],
 	providers: [AppService, ConfigService]
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+
+import { AppConfig, appConfig } from './app.config';
 
 @Injectable()
 export class AppService {
+	constructor(@Inject(appConfig.KEY) private readonly config: ConfigType<AppConfig>) {}
+
 	getHello(): string {
+		const host = this.config.host;
+		const port = this.config.port;
+
+		console.log('###', {
+			host,
+			port
+		});
+
 		return 'slack clone api server';
 	}
 }
